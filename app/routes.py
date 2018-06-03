f1ee37ecf192e89db3f9458c31210e401eeb5dcd
carawarner <warnercara@gmail.com>
2018-06-03 11:53:22 -0400

    Render a template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='static')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
