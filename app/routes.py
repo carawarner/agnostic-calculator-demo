49052a68f9d7ee950f06c0cd5f7b5d7f6f9e73d5
carawarner <warnercara@gmail.com>
2018-06-03 12:19:56 -0400

    Get Flask BE to render React FE

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='static')
+app = Flask(__name__, static_folder='static/dist', template_folder='static')
 
 @app.route('/')
 def hello_world():
